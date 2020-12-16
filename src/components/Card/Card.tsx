c0c1d9fe72bcb8b729c6e633029b45fb560aa115
pchalupa <chalupa.petr93@gmail.com>
2020-12-16 21:13:02 +0100

    Add card component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './style.module.css';
+
+interface ICard {
+	title?: string;
+	action?: JSX.Element;
+	children?: JSX.Element;
+}
+
+const Card: React.FC<ICard> = (props): JSX.Element => {
+	const { title, action, children } = props;
+
+	return (
+		<section className={styles.container}>
+			<div className={styles.header}>
+				<div className={styles.title}>{title}</div>
+				<div className={styles.action}>{action}</div>
+			</div>
+			<div className={styles.body}>{children}</div>
+		</section>
+	);
+};
+
+export default Card;
