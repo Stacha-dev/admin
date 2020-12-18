1ce9878263f0311d463b6f5545f9b975ce5843f8
pchalupa <chalupa.petr93@gmail.com>
2020-12-18 17:24:25 +0100

    Update header structure

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import styles from './styles.module.css';
 
 interface IHeader {
-	children?: JSX.Element | JSX.Element[];
+	children?: any;
 }
 
 const Header: React.FC<IHeader> = (props): JSX.Element => {
 	const { children } = props;
-	return <header className={styles.container}>{children}</header>;
+	return (
+		<header className={styles.container}>
+			<div className={styles.logo}>Logo</div>
+			{children}
+		</header>
+	);
 };
 
 export default Header;
