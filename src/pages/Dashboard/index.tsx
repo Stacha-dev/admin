7afd864b808bfa54baa3dc85cc0fd697ba7b59df
pchalupa <chalupa.petr93@gmail.com>
2020-12-18 17:25:54 +0100

    Add dashboard page

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card from '../../components/Card';
+
+const Dashboard: React.FC = (): JSX.Element => {
+	return (
+		<div>
+			<Card title="Dashboard">
+				<p>tady bude obsah</p>
+			</Card>
+		</div>
+	);
+};
+
+export default Dashboard;
