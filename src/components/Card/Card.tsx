99741fae2972b420ae24ce25d152f4e6597c97d6
pchalupa <chalupa.petr93@gmail.com>
2020-12-18 21:07:52 +0100

    Hide title if not provided

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -3,19 +3,19 @@ import styles from './style.module.css';
 
 interface ICard {
 	title?: string;
-	action?: JSX.Element;
 	children?: JSX.Element | JSX.Element[];
 }
 
 const Card: React.FC<ICard> = (props): JSX.Element => {
-	const { title, action, children } = props;
+	const { title, children } = props;
 
 	return (
 		<section className={styles.container}>
-			<div className={styles.header}>
-				<div className={styles.title}>{title}</div>
-				<div className={styles.action}>{action}</div>
-			</div>
+			{title && (
+				<div className={styles.header}>
+					<div className={styles.title}>{title}</div>
+				</div>
+			)}
 			<div className={styles.body}>{children}</div>
 		</section>
 	);
