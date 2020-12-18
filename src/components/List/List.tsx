ed0e70bde4029b899018f784d4a3fc7e19a20d73
pchalupa <chalupa.petr93@gmail.com>
2020-12-18 17:24:41 +0100

    Add list component

---
+++ src/components/List/List.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+interface IList {
+	data?: object[];
+	columns: { key: string; render?: (item: any) => void }[];
+}
+
+const List: React.FC<IList> = (props): JSX.Element => {
+	const { data, columns } = props;
+
+	return (
+		<div className={styles.container}>
+			{data?.map((row: any) => (
+				<div className={styles.row} key={row.id} style={{ gridTemplateColumns: '1fr '.repeat(columns.length) }}>
+					{columns.map((column, index) => (
+						<div key={index}>{column.render ? column.render(row[column.key]) : row[column.key]}</div>
+					))}
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default List;
