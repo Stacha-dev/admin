02e0ff2b77a2ae2964d08594564ab54faac4319a
pchalupa <chalupa.petr93@gmail.com>
2020-12-18 17:59:33 +0100

    Add style prop to button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,13 +5,14 @@ interface IButton {
 	text: string;
 	type: 'primary' | 'secondary';
 	onClick?: () => void;
+	style?: object;
 }
 
 const Button: React.FC<IButton> = (props): JSX.Element => {
-	const { text, type, onClick } = props;
+	const { text, type, onClick, style } = props;
 
 	return (
-		<button className={`${styles.container} ${styles[type]}`} onClick={onClick}>
+		<button className={`${styles.container} ${styles[type]}`} onClick={onClick} style={style}>
 			{text}
 		</button>
 	);
