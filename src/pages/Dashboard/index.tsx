0d29b855ff60488b39598075f48ce2095604d873
pchalupa <chalupa.petr93@gmail.com>
2020-12-20 17:21:25 +0100

    Use page component

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import Page from '../../components/Page';
 import Card from '../../components/Card';
+import styles from './styles.module.css';
 
 const Dashboard: React.FC = (): JSX.Element => {
 	return (
-		<div>
-			<Card title="Dashboard">
+		<Page>
+			<Card title="Dashboard" className={styles.dashboard}>
 				<p>tady bude obsah</p>
 			</Card>
-		</div>
+		</Page>
 	);
 };
 
