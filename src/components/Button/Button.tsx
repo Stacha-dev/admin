8c3f12a67aa0fe659832108a6d6399f767a78436
pchalupa <chalupa.petr93@gmail.com>
2021-01-30 08:59:02 +0100

    Refactoring

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import styles from './style.module.css';
 
-interface IButton {
+interface ButtonProps {
 	text: string;
 	type: 'primary' | 'secondary';
 	onClick?: () => void;
 	style?: object;
 }
 
-const Button: React.FC<IButton> = (props): JSX.Element => {
+const Button = (props: ButtonProps): JSX.Element => {
 	const { text, type, onClick, style } = props;
 
 	return (
