94c31d03b78b7315f1de86233851b8746e5918c0
pchalupa <chalupa.petr93@gmail.com>
2021-02-02 09:14:13 +0100

    Remove react ga

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import GA from 'react-ga';
-import { Metric } from 'web-vitals';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './locales';
@@ -14,19 +12,4 @@ ReactDOM.render(
 	document.getElementById('root')
 );
 
-GA.initialize('G-2FY8R8YP44', { debug: process.env.NODE_ENV === 'development' });
-GA.pageview(window.location.pathname + window.location.search);
-
-const sendToAnalytics = (metric: Metric) => {
-	const { id, name, value } = metric;
-
-	GA.event({
-		category: 'Web Vitals',
-		action: name,
-		label: id.toString(),
-		value: Math.round(name === 'CLS' ? value * 1000 : value),
-		nonInteraction: true,
-	});
-};
-
-reportWebVitals(sendToAnalytics);
+reportWebVitals();
