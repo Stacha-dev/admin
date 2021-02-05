f14d193e1e3a3823e2e5af8705ccfbfd420f6385
pchalupa <chalupa.petr93@gmail.com>
2021-02-05 19:57:57 +0100

    Add default value prop

--- src/components/Form/Fields/Input.tsx
+++ src/components/Form/Fields/Input.tsx
@@ -6,17 +6,26 @@ interface InputProps {
 	label?: string;
 	name: string;
 	value?: string;
+	defaultValue?: string;
 	type: InputType;
 	placeholder?: string;
 }
 
 const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
-	const { label, name, value, type, placeholder } = props;
+	const { label, name, value, defaultValue, type, placeholder } = props;
 
 	return (
 		<label className={styles.label}>
 			<span className={styles.labelText}>{label}</span>
-			<input className={styles.input} type={type} placeholder={placeholder} name={name} value={value} ref={ref} />
+			<input
+				className={styles.input}
+				type={type}
+				placeholder={placeholder}
+				name={name}
+				value={value}
+				defaultValue={defaultValue}
+				ref={ref}
+			/>
 		</label>
 	);
 });
