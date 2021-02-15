beee4fcad56f0ef3faf73c6b1a27b7884a38943f
pchalupa <chalupa.petr93@gmail.com>
2021-02-15 19:10:28 +0100

    Add welcome message

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import Card from '../../components/Card';
 import styles from './styles.module.css';
 
 const Dashboard = (): JSX.Element => {
-	return (
-		<>
-			<Card title="Dashboard" className={styles.dashboard}>
-				<p>tady bude obsah</p>
-			</Card>
-		</>
-	);
+	const { t } = useTranslation('page');
+
+	return <Card title={t('dashboard.welcomeBack')} className={styles.dashboard}></Card>;
 };
 
 export default Dashboard;
