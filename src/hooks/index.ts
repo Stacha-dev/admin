6919a2fca2830d34e2fb82341b4bb122b02b2bc9
pchalupa <chalupa.petr93@gmail.com>
2021-03-10 20:14:23 +0100

    Export service

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,7 +3,17 @@ import useUser from './useUser';
 import useGalleryService from './useGalleryService';
 import useImageService from './useImageService';
 import useUserService from './useUserService';
+import useMenuService from './useMenuService';
 import useToast from './useToast';
 import useAnimation from './useAnimation';
 
-export { useLoading, useUser, useGalleryService, useImageService, useUserService, useToast, useAnimation };
+export {
+	useLoading,
+	useUser,
+	useGalleryService,
+	useImageService,
+	useUserService,
+	useMenuService,
+	useToast,
+	useAnimation,
+};
