a49574285378b05487cf3a67559cce96fd9d6111
pchalupa <chalupa.petr93@gmail.com>
2021-03-10 20:21:23 +0100

    Remove config

--- src/configuration.ts
+++
@@ -1,6 +0,0 @@
-export const menu = [
-	{ name: 'menu.slideshow', path: '/gallery/1' },
-	{ name: 'menu.realizations', path: '/gallery/tag/2' },
-	{ name: 'menu.media', path: '/gallery/tag/3' },
-	{ name: 'menu.press', path: '/gallery/tag/4' },
-];
