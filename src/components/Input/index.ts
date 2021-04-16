ffe39a8f5767675f717fc46dbab793e5b98737e4
pchalupa <chalupa.petr93@gmail.com>
2021-04-16 22:18:38 +0200

    Add textarea component

--- src/components/Input/index.ts
+++ src/components/Input/index.ts
@@ -1,6 +1,7 @@
 import Input from './Input';
+import Textarea from './Textarea';
 import FileInput from './FileInput';
 import Switch from './Switch';
 import Submit from './Submit';
 
-export { Input, FileInput, Switch, Submit };
+export { Input, Textarea, FileInput, Switch, Submit };
