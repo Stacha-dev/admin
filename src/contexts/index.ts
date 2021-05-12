0f41a6dcda5d46c782bd9e73e50feecd121fc009
pchalupa <chalupa.petr93@gmail.com>
2021-05-12 19:56:35 +0200

    Wrap app with context

--- src/contexts/index.ts
+++ src/contexts/index.ts
@@ -1,5 +1,15 @@
 import UserContext, { UserProvider } from './UserContext';
 import LoadingContext, { LoadingProvider } from './LoadingContext';
 import ToastContext, { ToastProvider } from './ToastContext';
+import LocaleContext, { LocaleProvider } from './LocaleContext';
 
-export { UserContext, UserProvider, LoadingContext, LoadingProvider, ToastContext, ToastProvider };
+export {
+	UserContext,
+	UserProvider,
+	LoadingContext,
+	LoadingProvider,
+	ToastContext,
+	ToastProvider,
+	LocaleContext,
+	LocaleProvider,
+};
